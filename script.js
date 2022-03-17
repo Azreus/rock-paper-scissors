d307d822cd50f3accb2b4ea1d3b9149391ed07dc
Justin <justin27sackprasit@gmail.com>
2022-03-17 23:42:44 +1100

    Create playRound function
    Create the playRound() function that determines the outcome of the game
    based on the playerSelection and computerSelection arguments.
    
    The function utilises if/else conditional statements and returns a
    string in the console that states who wins the round.
    
    Beforehand, a prompt for user input was created, and a function to
    capitalise that string is used to make the value remain consistent with
    the playRound() conditionals.

--- script.js
+++ script.js
@@ -10,4 +10,29 @@ function computerPlay() {
   }
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "Paper" && computerSelection == "Rock" ||
+  playerSelection == "Rock" && computerSelection == "Scissors" ||
+  playerSelection == "Scissors" && computerSelection == "Paper") 
+  {
+    return `You win! ${playerSelection} beats ${computerSelection}`; 
+  }
+  else if (playerSelection == "Rock" && computerSelection == "Paper" ||
+  playerSelection == "Paper" && computerSelection == "Scissors" ||
+  playerSelection == "Scissors" && computerSelection == "Rock")
+  {
+    return `You lose... ${computerSelection} beats ${playerSelection}`;  
+  }
+  else { return "It's a tie!"; }
+}
+
+function capitaliseFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+let playerInputA = prompt("Rock, paper, or scissors?");
+let playerInput = capitaliseFirstLetter(playerInputA);
+
+
+
+console.log(playRound(playerInput, computerPlay()));
